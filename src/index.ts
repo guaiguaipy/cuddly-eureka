7fa5100212975f0ba0a1f7acc26770bb6d29efd2
guaiguaipy <67051488+guaiguaipy@users.noreply.github.com>
2020-11-12 13:16:22 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+/*! Copyright (c) 2020, XAPP AI */
+// Loads the environment variables from a .env file at root
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config();
+
+// Assistant
+import { Assistant } from "stentor";
+
+// Channels
+import { Alexa } from "@xapp/stentor-alexa";
+import { Dialogflow } from "@xapp/stentor-dialogflow";
+import { LexConnect } from "@xapp/stentor-lex-connect";
+
+// Services
+import { DynamoUserStorage } from "stentor-user-storage-dynamo";
+
+// Custom Handlers
+import { QuestionAnsweringHandler } from "@xapp/question-answering-handler";
+
+// Return the handler for running in an AWS Lambda function.
+export const handler: AWSLambda.Handler = new Assistant()
+    .withUserStorage(new DynamoUserStorage())
+    .withHandlers({
+        QuestionAnsweringHandler: QuestionAnsweringHandler
+    })
+    .withChannels([Alexa(), Dialogflow(), LexConnect()])
+    .lambda();
